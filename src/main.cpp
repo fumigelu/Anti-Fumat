432d623b5abea777dabcbed65a5e0ca448ba9bbd
Detector Fum <detectorfum@outlook.com>
2024-02-10 22:05:47 +0200

    DHT example

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,47 @@
-#include <Arduino.h>
+#include <SimpleDHT.h>
 
-int led = 5;
+// for DHT22,
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT22 = 22;
+SimpleDHT11 dht22(pinDHT22);
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(led, OUTPUT); 
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(led, HIGH); 
-delay(1000); 
-digitalWrite(led, LOW); 
-delay(1000); 
+  // start working...
+  Serial.println("=================================");
+  Serial.println("Sample DHT22 with RAW bits...");
+  
+  // read with raw sample data.
+  // @remark We use read2 to get a float data, such as 10.1*C
+  //    if user doesn't care about the accurate data, use read to get a byte data, such as 10*C.
+  float temperature = 0;
+  float humidity = 0;
+  byte data[40] = {0};
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht22.read2(&temperature, &humidity, data)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT22 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(2000);
+    return;
+  }
+  
+  Serial.print("Sample RAW Bits: ");
+  for (int i = 0; i < 40; i++) {
+    Serial.print((int)data[i]);
+    if (i > 0 && ((i + 1) % 4) == 0) {
+      Serial.print(' ');
+    }
+  }
+  Serial.println("");
+  
+  Serial.print("Sample OK: ");
+  Serial.print((float)temperature); Serial.print(" *C, ");
+  Serial.print((float)humidity); Serial.println(" RH%");
+  
+  // DHT22 sampling rate is 0.5HZ.
+  delay(2500);
 }
