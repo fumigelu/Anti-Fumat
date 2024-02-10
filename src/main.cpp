a08ac0dceeeb1c9d6b14db6108d11a5e4417158d
Detector Fum <detectorfum@outlook.com>
2024-02-11 01:30:41 +0200

    Sending values to the app

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,58 @@
+/*************************************************************
+
+  This example shows how value can be pushed from Arduino to
+  the Blynk App.
+
+  WARNING :
+  For this example you'll need Adafruit DHT sensor libraries:
+    https://github.com/adafruit/Adafruit_Sensor
+    https://github.com/adafruit/DHT-sensor-library
+
+  App dashboard setup:
+    Value Display widget attached to V5
+    Value Display widget attached to V6
+ *************************************************************/
+
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID           "TMPL4VNHe72Y1"
+#define BLYNK_TEMPLATE_NAME         "Quickstart Device"
+#define BLYNK_AUTH_TOKEN            "QCRfgaoUZTVbLmFCFspuIIvmoEYuS9Ie"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 #include <SimpleDHT.h>
 
-// for DHT11,
-//      VCC: 5V or 3V
-//      GND: GND
-//      DATA: 22
-int pinDHT11 = 22;
-SimpleDHT11 dht22(pinDHT11);
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "POCO X3 PRO";
+char pass[] = "neagu1234";
 
-void setup() {
-  Serial.begin(9600);
-}
+#define DHTPIN 22          // What digital pin we're connected to
 
-void loop() {
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11     // DHT 11
+//#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
+//#define DHTTYPE DHT21   // DHT 21, AM2301
+
+SimpleDHT11 dht11(DHTPIN);
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void sendSensor()
+{
   float temperature = 0;
   float humidity = 0;
   byte data[40] = {0};
   int err = SimpleDHTErrSuccess;
   
-  if ((err = dht22.read2(&temperature, &humidity, data)) != SimpleDHTErrSuccess) {
+  if ((err = dht11.read2(&temperature, &humidity, data)) != SimpleDHTErrSuccess) {
     Serial.print("Read DHT22 failed, err="); Serial.print(SimpleDHTErrCode(err));
     Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(2000);
     return;
@@ -26,7 +61,29 @@ void loop() {
   Serial.print("Sample OK: ");
   Serial.print((float)temperature); Serial.print(" *C, ");
   Serial.print((float)humidity); Serial.println(" RH%");
-  
-  // DHT11 sampling rate is 1HZ.
-  delay(5000);
+
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, (float)humidity);
+  Blynk.virtualWrite(V6, (float)temperature);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
 }
